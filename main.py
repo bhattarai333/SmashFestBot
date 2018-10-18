dbfff364961ba790fbc0ada384faeb499bad880b
Josh Bhattarai <bhattar2@msu.edu>
2018-10-18 19:31:59 -0400

    misc

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
+    pass
 
 @app.route('/recommendation', methods=['POST'])
 def get_recommendation():
-
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
