e4413fcee2450009fb6f674d10a458aaf58eabea
Josh Bhattarai <bhattar2@msu.edu>
2018-10-19 04:09:34 -0400

    misc

--- main.py
+++ main.py
@@ -5,19 +5,71 @@ import asyncio
 import time
 import os
 
+
+
+
+
+
+
+class SmashFest:
+    owner = "MSU | BlueFlare10"
+    participants = list()
+    location = "Communication Arts Building, Room 154"
+    startTime = "6:00 PM"
+    currentSetups = 0
+    numSetups = 0
+
+    def __init__(self, owner, location, startTime):
+        self.owner = owner
+        self.location = location
+        self.startTime = startTime
+
+    def addParticipants(self, participant):
+        self.participants.append(participant)
+
+
+
+
+
+
+
 Client = discord.Client()
 client = commands.Bot(command_prefix = ".")
 
+smashfests = list()
+
 @client.event
 async def on_ready():
-    print("hello")
-    await client.change_presence(game=discord.Game(name="videos"))
+    await client.change_presence(game=discord.Game(name="Smash Ultimate"))
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('.hello'):
+    if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention} How are you today?'.format(message)
-        await client.send_message(message.channel,msg)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith("!mario"):
+        msg = "J3's Mario is sick"
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith("pizza house"):
+        msg = "BDubs is better"
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith("!carter"):
+        msg = "Money match me {0.author.mention}, you won't".format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith("tommy"):
+        msg = "Tommy sux"
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!create'):
+        parts = str.split(message.content, '/')
+
+        sf = SmashFest(message.author, parts[0], parts[1])
+        smashfests.append(sf)
+        msg = str(sf.owner) + str(sf.location) + str(sf.startTime)
+        await client.send_message(message.channel, msg)
 
 client.run('NTAyNTg5MzM2NzA2MDg4OTYy.Dqqr3w.vQdTF0dW6yiT8e8X_e8ZqcCEF1w')
-print('running')
