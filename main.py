94ce944028cc118af272216974fb32189bc4ef54
Josh Bhattarai <bhattar2@msu.edu>
2018-10-22 01:16:38 -0400

    misc

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ async def on_message(message):
         await client.send_message(message.channel, "!monitors/SMASHFEST NUMBER                                                         Displays how many monitors are at a fest")
         await client.send_message(message.channel, "!end/SMASHFEST NUMBER                                                              Ends a smashfest")
     if message.content.startswith("!about"):
-        msg = "Hello, I am a bot to help MSU students find places to play Smash 4/Smash Ultimate on campus. Direct any complaints to J3.\n"
+        msg = "Hello, I am a bot to help MSU students find places to play Smash 4/Smash Ultimate on campus. Direct any complaints to J3(Josh Bhattarai).\n"
         msg = msg + "Written in Python 3.7, running in a free hosted Heroku Python 3.6.6 enviornment. Uses Discord.py by Rapptz\n"
         msg = msg + "Please follow the MSU Smash 4 Twitter: https://twitter.com/msusmash4"
         await client.send_message(message.channel, msg)
