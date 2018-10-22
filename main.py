719da4f00d34268543e9a60ecf10379799477466
Josh Bhattarai <bhattar2@msu.edu>
2018-10-22 01:03:50 -0400

    misc

--- main.py
+++ main.py
@@ -193,6 +193,7 @@ async def on_message(message):
         msg = "Hello, I am a bot to help MSU students find places to play Smash 4/Smash Ultimate on campus. Direct any complaints to J3.\n"
         msg = msg + "Written in Python 3.7, running in a free hosted Heroku Python 3.6.6 enviornment. Uses Discord.py by Rapptz\n"
         msg = msg + "Follow the MSU Smash 4 Twitter: https://twitter.com/msusmash4"
+        await client.send_message(message.channel, msg)
 
 
 
