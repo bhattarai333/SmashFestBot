0abe060e32dd8c51e79afe5328e13e2d3db5ff8c
bhattarai333 <joshbhattarai1@gmail.com>
2019-01-25 16:53:45 -0500

    misc

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ Client = discord.Client()
 client = commands.Bot(command_prefix = "!")
 
 bdubs_emoji = "Yay BDubs"
-version = "**SmashFest Bot v3.1.5**"
+version = "**SmashFest Bot v3.1.6**"
 pickle_counter = 0
 
 
